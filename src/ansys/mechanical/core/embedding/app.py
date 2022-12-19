2767e4e870346f4bb0eea9a256af2a6ce9daccd4
Mohamed Koubaa <koubaa.m@gmail.com>
2022-12-19 09:05:38 -0600

    Embedding (#68)

---
+++ src/ansys/mechanical/core/embedding/app.py
@@ -0,0 +1,94 @@
+"""Main application class for embedded Mechanical."""
+import os
+
+import clr
+
+from . import initializer
+
+INITIALIZED = False
+
+
+def _get_available_versions():
+    supported_versions = [222, 231, 232]
+    if os.name == "nt":
+        awp_roots = {ver: os.environ.get(f"AWP_ROOT{ver}", "") for ver in supported_versions}
+        installed_versions = {
+            ver: path for ver, path in awp_roots.items() if path and os.path.isdir(path)
+        }
+        return installed_versions
+    else:
+        raise Exception("TODO - Support linux")
+
+
+def _get_default_version():
+    vers = _get_available_versions()
+    if not vers:
+        raise Exception(
+            "Appropriate version of Ansys Mechanical is not installed. Must be at least v222"
+        )
+    return max(vers.keys())
+
+
+class App:
+    """Mechanical embedding Application."""
+
+    def __init__(self, db_file=None, **kwargs):
+        """Construct an instance of the mechanical Application.
+
+        db_file is an optional path to a mechanical database file (.mechdat or .mechdb)
+        you may set a version number with the `version` keyword argument.
+        """
+        global INITIALIZED
+        if INITIALIZED:
+            raise Exception("Cannot initialize embedded mechanical more than once")
+        version = kwargs.get("version")
+        if version == None:
+            version = _get_default_version()
+        initializer.initialize(version)
+        clr.AddReference("Ansys.Mechanical.Embedding")
+        if version >= 231:
+            clr.AddReference("Ansys.Mechanical.CPython")
+        import Ansys
+
+        self._app = Ansys.Mechanical.Embedding.Application(db_file)
+        if version >= 231:
+            Ansys.Mechanical.CPython.CPythonEngine.InitializeRuntime()
+        INITIALIZED = True
+
+    def __enter__(self):
+        """Enter the scope."""
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Exit the scope."""
+        self._app.Dispose()
+
+    def open(self, db_file):
+        """Open the db file."""
+        self.DataModel.Project.Open(db_file)
+
+    def save(self, path=None):
+        """Save the project."""
+        self.DataModel.Project.Save(path)
+
+    def save_as(self, path):
+        """Save the project as."""
+        self.DataModel.Project.SaveAs(path)
+
+    def new(self):
+        """Clear to a new application."""
+        self.DataModel.Project.New()
+
+    def close(self):
+        """Close the application."""
+        self.ExtAPI.Application.Close()
+
+    @property
+    def DataModel(self):
+        """Return the DataModel."""
+        return self._app.DataModel
+
+    @property
+    def ExtAPI(self):
+        """Return the ExtAPI object."""
+        return self._app.ExtAPI
