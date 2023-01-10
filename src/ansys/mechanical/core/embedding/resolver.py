f2010a1efdae6e25d617fd0cd66012babda0db6d
Mohamed Koubaa <mkoubaa@chqmechlinux01.ansys.com>
2023-01-10 15:36:02 -0500

    fix

--- src/ansys/mechanical/core/embedding/resolver.py
+++ src/ansys/mechanical/core/embedding/resolver.py
@@ -83,7 +83,11 @@ def resolve(version):
         if version == 231:
             resolve_handler = assembly_resolver.WindowsResolveEventHandler
         else:
-            resolve_handler = assembly_resolver.WindowsResolveEventHandler
+            try:
+                resolve_handler = assembly_resolver.MechanicalResolveEventHandler
+            except:
+                #TODO - remove me, once people are on a newer build
+                resolve_handler = assembly_resolver.WindowsResolveEventHandler
         System.AppDomain.CurrentDomain.AssemblyResolve += resolve_handler
     else:
         System.AppDomain.CurrentDomain.AssemblyResolve += resolver222()
