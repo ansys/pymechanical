29f29eae5750ffa58699bb60970ec88b40a28b3b
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-02-17 10:00:03 +0100

    Removing CI file

--- .ci/start_mechanical.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker pull $MECHANICAL_IMAGE
-docker run --restart always --name mechanical -e ANSYSLMD_LICENSE_FILE=1055@$LICENSE_SERVER -p $PYMECHANICAL_PORT:10000 $MECHANICAL_IMAGE > log.txt &
-grep -q 'WB Initialize Done' <(timeout 60 tail -f log.txt)
