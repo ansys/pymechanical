5ef8b387a42ec8179fac72b5598f6dfade3efd4e
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-03-06 10:39:25 +0100

    Updating versions.json location

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -177,7 +177,7 @@ html_context = {
 }
 html_theme_options = {
     "switcher": {
-        "json_url": f"https://{cname}/release/versions.json",
+        "json_url": f"https://{cname}/versions.json",
         "version_match": get_version_match(version),
     },
     "check_switcher": False,
