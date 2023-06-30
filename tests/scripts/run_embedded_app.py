27e6a759c3149563582df0196e9a5c1942cc1a4d
klmcadams <58492561+klmcadams@users.noreply.github.com>
2023-06-30 11:10:47 -0400

    Added private AppData functionality to embedding (#285)

--- tests/scripts/run_embedded_app.py
+++ tests/scripts/run_embedded_app.py
@@ -1,5 +1,41 @@
 """Launch embedded instance."""
+import sys
+
 import ansys.mechanical.core as pymechanical
 
-# Launch embedded instance of app
-app = pymechanical.App()
+
+def launch_app(appdata_option):
+    """Launch embedded instance of app."""
+    if appdata_option == "True":
+        app = pymechanical.App(private_appdata=appdata_option)
+    else:
+        app = pymechanical.App()
+    return app
+
+
+def set_showtriad(appdata_option, value):
+    """Launch embedded instance of app & set ShowTriad to False."""
+    app = launch_app(appdata_option)
+    app.ExtAPI.Graphics.ViewOptions.ShowTriad = value
+    app.close()
+
+
+def print_showtriad(appdata_option):
+    """Return ShowTriad value."""
+    app = launch_app(appdata_option)
+    print(app.ExtAPI.Graphics.ViewOptions.ShowTriad)
+    app.close()
+
+
+try:
+    appdata_option = sys.argv[1]
+    action = sys.argv[2]
+
+    if action == "Set":
+        set_showtriad(appdata_option, False)
+    elif action == "Run":
+        print_showtriad(appdata_option)
+    elif action == "Reset":
+        set_showtriad(appdata_option, True)
+except:
+    launch_app("")
