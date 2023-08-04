1882fc2250bd12e845bb21ef7b4738d2d3d0758a
Mohamed Koubaa <koubaa@github.com>
2023-08-04 13:33:48 -0500

    first version

---
+++ future/Enums.cs
@@ -0,0 +1,15 @@
+﻿using Ansys.Common.Interop.DSObjectTypes;
+
+namespace Ansys.Mechanical.Future.Enums
+{
+    public enum GeometryType
+    {
+        Solid = DSGeometryType.kSolidGeometry,
+        Sheet = DSGeometryType.kSheetGeometry
+    }
+
+    public enum TransferFileFormat
+    {
+        HDF5 = 0
+    }
+}
---
+++ future/ModelExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Ansys.ACT.Automation.Mechanical;
+using Ansys.Common.Interop.DSObjectTypes;
+using Ansys.Mechanical.DataModel.Enums;
+
+namespace Ansys.Mechanical.Future
+{
+    public class ModelHDF5ExportSettings
+    {
+        public Enums.GeometryType GeometryType { get; set; }
+        public WBUnitSystemType UnitSystemType { get; set; }
+    }
+
+    public static class ModelExtensions
+    {
+        public static void ExportHDF5TransferFile(
+            this Model model,
+            string filename,
+            Enums.TransferFileFormat format,
+            ModelHDF5ExportSettings settings)
+        {
+            if (format != Enums.TransferFileFormat.HDF5)
+                throw new System.Exception("Unknown format");
+            model.InternalObject.WriteHDF5TransferFile((DSGeometryType)settings.GeometryType, filename, (int)settings.UnitSystemType, 0);
+        }
+    }
+}
