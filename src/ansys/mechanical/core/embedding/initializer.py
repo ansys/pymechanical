bd1547bb6e9deb3a61ebb86e15904ae4d5512fdb
Mohamed Koubaa <koubaa@github.com>
2023-08-07 11:39:55 -0500

    Use an easier to find path for private appdata

--- src/ansys/mechanical/core/embedding/initializer.py
+++ src/ansys/mechanical/core/embedding/initializer.py
@@ -122,7 +122,7 @@ def _get_default_version() -> int:
 def set_private_appdata(pid):
     """Set up unique user sessions when running parallel instances."""
     defaultProfile = os.path.expanduser("~")
-    profileName = rf"{os.path.expanduser( '~' )}{pid}"
+    profileName = os.path.join(defaultProfile, f"PyMechanical-{pid}")
 
     newProfile = TmpUser(defaultProfile, profileName)
 
