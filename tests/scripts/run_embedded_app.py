2b47023ad535b8cd1d13192a546aea3a7a9e8c98
Mohamed Koubaa <koubaa@github.com>
2023-08-07 11:40:49 -0500

    rework how run_embedded_app is used
    pass in the version number
    don't use exceptions for flow control
    don't use a virtual environment for appdata tests

--- tests/scripts/run_embedded_app.py
+++ tests/scripts/run_embedded_app.py
@@ -4,38 +4,39 @@ import sys
 import ansys.mechanical.core as pymechanical
 
 
-def launch_app(appdata_option):
+def launch_app(version, private_appdata):
     """Launch embedded instance of app."""
-    if appdata_option == "True":
-        app = pymechanical.App(private_appdata=appdata_option)
-    else:
-        app = pymechanical.App()
+    app = pymechanical.App(version=version, private_appdata=private_appdata)
     return app
 
 
-def set_showtriad(appdata_option, value):
+def set_showtriad(version, appdata_option, value):
     """Launch embedded instance of app & set ShowTriad to False."""
-    app = launch_app(appdata_option)
+    app = launch_app(version, appdata_option)
     app.ExtAPI.Graphics.ViewOptions.ShowTriad = value
     app.close()
 
 
-def print_showtriad(appdata_option):
+def print_showtriad(version, appdata_option):
     """Return ShowTriad value."""
-    app = launch_app(appdata_option)
+    app = launch_app(version, appdata_option)
     print(app.ExtAPI.Graphics.ViewOptions.ShowTriad)
     app.close()
 
 
-try:
-    appdata_option = sys.argv[1]
-    action = sys.argv[2]
+if __name__ == "__main__":
+    version = sys.argv[1]
+    if len(sys.argv) == 2:
+        launch_app(version, False)
+        sys.exit(0)
 
+    appdata_option = sys.argv[2]
+    action = sys.argv[3]
+
+    private_appdata = appdata_option == "True"
     if action == "Set":
-        set_showtriad(appdata_option, False)
+        set_showtriad(version, private_appdata, False)
     elif action == "Run":
-        print_showtriad(appdata_option)
+        print_showtriad(version, private_appdata)
     elif action == "Reset":
-        set_showtriad(appdata_option, True)
-except:
-    launch_app("")
+        set_showtriad(version, private_appdata, True)
