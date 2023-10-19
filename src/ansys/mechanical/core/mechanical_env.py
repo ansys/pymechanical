8ad067195c7a62fe358a4aeff00702de4c320535
Kerry McAdams <kerry.mcadams@ansys.com>
2023-10-19 13:08:36 +0000

    adjust variable names & remove print statements

--- src/ansys/mechanical/core/mechanical_env.py
+++ src/ansys/mechanical/core/mechanical_env.py
@@ -29,7 +29,7 @@ import sys
 from ansys.tools.path import find_mechanical
 
 
-def create_env(version, aisol_path):
+def create_env(revn, aisol_path):
     """Set up the environment in Linux."""
     proc_env = os.environ.copy()
 
@@ -37,21 +37,21 @@ def create_env(version, aisol_path):
     proc_env["DS_INSTALL_DIR"] = str(aisol_path)
     ds_install_dir = proc_env.get("DS_INSTALL_DIR")
     # /install/ansys_inc/v241
-    proc_env[f"AWP_ROOT{version}"] = f"{ds_install_dir}/../"
+    proc_env[f"AWP_ROOT{revn}"] = f"{ds_install_dir}/.."
 
-    awp_root = proc_env.get(f"AWP_ROOT{version}")
+    awp_root = proc_env.get(f"AWP_ROOT{revn}")
 
     # Environment variables used by workbench (mechanical) code
-    proc_env[f"AWP_LOCALE{version}"] = "en-us"
-    awp_locale = proc_env.get(f"AWP_LOCALE{version}")
+    proc_env[f"AWP_LOCALE{revn}"] = "en-us"
+    awp_locale = proc_env.get(f"AWP_LOCALE{revn}")
     # /install/ansys_inc/v241/commonfiles/language/en-us
-    proc_env[f"CADOE_LIBDIR{version}"] = f"{awp_root}/commonfiles/language/{awp_locale}"
+    proc_env[f"CADOE_LIBDIR{revn}"] = f"{awp_root}/commonfiles/language/{awp_locale}"
     # /install/ansys_inc/shared_files/licensing
     proc_env["ANSYSLIC_DIR"] = f"{awp_root}/../shared_files/licensing"
     # /install/ansys_inc/v241/commonfiles
     proc_env["ANSYSCOMMON_DIR"] = f"{awp_root}/commonfiles"
     # /install/ansys_inc/v241/licensingclient
-    proc_env[f"ANSYSCL{version}_DIR"] = f"{awp_root}/licensingclient"
+    proc_env[f"ANSYSCL{revn}_DIR"] = f"{awp_root}/licensingclient"
 
     # MainWin variables
     home = proc_env.get("HOME")
@@ -77,12 +77,11 @@ def create_env(version, aisol_path):
     ld_library_path = proc_env.get("LD_LIBRARY_PATH")
     proc_env[
         "LD_LIBRARY_PATH"
-    ] = f"""
-{awp_root}/tp/stdc++:\
+    ] = f"""{awp_root}/tp/stdc++:\
 {awp_root}/tp/openssl/1.1.1/linx64/lib:\
 {mwhome}/lib-amd64_linux:\
 {mwhome}/lib-amd64_linux_optimized:\
-{ld_library_path}:\
+{ld_library_path}\
 {awp_root}/Tools/mono/Linux64/lib:\
 {ds_install_dir}/lib/linx64:\
 {ds_install_dir}/dll/linx64:\
@@ -98,14 +97,11 @@ def create_env(version, aisol_path):
     path = proc_env.get("PATH")
     proc_env[
         "PATH"
-    ] = f"""
-{mwhome}/bin-amd64_linux_optimized:\
+    ] = f"""{mwhome}/bin-amd64_linux_optimized:\
 {ds_install_dir}/CommonFiles/linx64:\
 {ds_install_dir}/CADIntegration/linx64:\
 {awp_root}/Tools/mono/Linux64/bin:\
 {path}"""
-
-    print(f"proc_env: {proc_env}")
 
     return proc_env
 
@@ -134,8 +130,8 @@ def main():
         print("mechanical-env is only available for Linux.")
     else:
         # Get revision number
-        exe, version = find_mechanical()
-        default_revn = int(version * 10)
+        exe, revn = find_mechanical()
+        revn = int(revn * 10)
 
         parser = argparse.ArgumentParser()
         # Get command from stdin
@@ -146,18 +142,18 @@ def main():
             help='Ansys Revision number, e.g. "241" or "232".\
             If a revision number is not specified, it uses the default from \
             ansys-tools-path.',
-            default=default_revn,
+            default=revn,
         )
 
         args = parser.parse_args()
 
         # If the user chooses a revision number, find the mechanical install of the given revision
-        if int(args.revision) != default_revn:
+        if int(args.revision) != revn:
             try:
-                exe, version = find_mechanical(version=int(args.revision))
-                version = int(version * 10)
+                exe, revn = find_mechanical(version=int(args.revision))
+                revn = int(revn * 10)
             except ValueError as e:
-                print(f"There was a ValueError for version {args.revision}.")
+                print(f"There was a ValueError for revn {args.revision}.")
                 exit(1)
 
         # Get aisol_path for Linux system
@@ -169,19 +165,12 @@ def main():
         else:
             cmd = args.command
 
-        before_env = os.environ
-
-        # If the version and aisol_path exist, run the command
-        if version and aisol_path:
-            proc_env = create_env(version, aisol_path)
-            # run_command(proc_env, cmd)
+        # If the revision number and aisol_path exist, run the command
+        if revn and aisol_path:
+            proc_env = create_env(revn, aisol_path)
+            run_command(proc_env, cmd)
         else:
-            print("There was a problem getting the version and aisol_path")
-
-        after_env = os.environ
-
-        print("is before_env the same as after_env?")
-        print(before_env == after_env)
+            print("There was a problem getting the revn and aisol_path")
 
 
 if __name__ == "__main__":
