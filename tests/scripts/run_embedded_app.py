8c3cfbe8e9c3a4730e17da3018c66bcc3d9192a8
Kerry McAdams <kerry.mcadams@ansys.com>
2024-01-29 10:20:24 -0500

    add timeout to pytest

--- tests/scripts/run_embedded_app.py
+++ tests/scripts/run_embedded_app.py
@@ -21,7 +21,6 @@
 # SOFTWARE.
 
 """Launch embedded instance."""
-import logging
 import sys
 
 import ansys.mechanical.core as pymechanical
@@ -30,14 +29,12 @@ import ansys.mechanical.core as pymechanical
 def launch_app(version, private_appdata):
     """Launch embedded instance of app."""
     app = pymechanical.App(version=version, private_appdata=private_appdata)
-    logging.error("Issue in launch_app %r", app)
     return app
 
 
 def set_showtriad(version, appdata_option, value):
     """Launch embedded instance of app & set ShowTriad to False."""
     app = launch_app(version, appdata_option)
-    logging.error("Issue in set_showtriad %r", app)
     app.ExtAPI.Graphics.ViewOptions.ShowTriad = value
     app.close()
 
@@ -45,7 +42,6 @@ def set_showtriad(version, appdata_option, value):
 def print_showtriad(version, appdata_option):
     """Return ShowTriad value."""
     app = launch_app(version, appdata_option)
-    logging.error("Issue in print_showtriad %r", app)
     print("ShowTriad value is " + str(app.ExtAPI.Graphics.ViewOptions.ShowTriad))
     app.close()
 
@@ -53,7 +49,6 @@ def print_showtriad(version, appdata_option):
 def reset_showtriad(version, appdata_option):
     """Reset ShowTriad value."""
     app = launch_app(version, appdata_option)
-    logging.error("Issue in reset_showtriad %r", app)
     app.ExtAPI.Graphics.ViewOptions.Reset
     app.close()
 
