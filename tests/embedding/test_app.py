12e4b66e985af025833540dabb74fdd924a36786
Kerry McAdams <kerry.mcadams@ansys.com>
2024-03-27 14:19:26 -0400

    check embedded_py path

--- tests/embedding/test_app.py
+++ tests/embedding/test_app.py
@@ -163,7 +163,7 @@ def test_app_getters_notstale(embedded_app):
 
 @pytest.mark.embedding_scripts
 @pytest.mark.python_env
-def test_warning_message(test_env, pytestconfig, rootdir):
+def test_warning_message(test_env, pytestconfig, run_subprocess, rootdir):
     """Test Python.NET warning of the embedded instance using a test-scoped Python environment."""
     # Install pymechanical
     subprocess.check_call(
@@ -177,12 +177,11 @@ def test_warning_message(test_env, pytestconfig, rootdir):
 
     # Run embedded instance in virtual env with pythonnet installed
     embedded_py = os.path.join(rootdir, "tests", "scripts", "run_embedded_app.py")
-    check_warning = subprocess.Popen(
-        [test_env.python, embedded_py, pytestconfig.getoption("ansys_version")],
-        stderr=subprocess.PIPE,
-        env=test_env.env,
+    print(embedded_py)
+    print(os.path.isfile(embedded_py))
+    _, stderr = run_subprocess(
+        [test_env.python, embedded_py, pytestconfig.getoption("ansys_version")]
     )
-    stdout, stderr = check_warning.communicate()
 
     # If UserWarning & pythonnet are in the stderr output, set warning to True.
     # Otherwise, set warning to False
