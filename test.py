35ff481d27867e222d2c1f49e1369e75b1d4bf5a
Mohamed Koubaa <koubaa@github.com>
2024-04-03 14:08:13 -0500

    oops

--- test.py
+++
@@ -1,6 +0,0 @@
-import ansys.mechanical.core as mech
-
-project_file = r"C:\Users\mkoubaa\Desktop\sanderson.mechdat"
-app = mech.App(version=242, db_file=project_file)
-app.open(project_file)
-app.plot()
