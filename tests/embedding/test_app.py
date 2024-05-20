7a1da280332ac28dd85123690cc354a35baf6003
Dipin <26918585+dipinknair@users.noreply.github.com>
2024-05-20 11:19:28 -0500

    feat: Add test for deprecation warning (#739)
    Co-authored-by: pyansys-ci-bot <pyansys.github.bot@ansys.com>

--- tests/embedding/test_app.py
+++ tests/embedding/test_app.py
@@ -44,9 +44,10 @@ def test_app_repr(embedded_app):
 @pytest.mark.embedding
 @pytest.mark.minimum_version(241)
 def test_deprecation_warning(embedded_app):
-    struct = embedded_app.Model.AddStaticStructuralAnalysis()
+    harmonic_acoustic = embedded_app.Model.AddHarmonicAcousticAnalysis()
     with pytest.warns(UserWarning):
-        struct.SystemID
+        harmonic_acoustic.SystemID
+        harmonic_acoustic.AnalysisSettings.MultipleRPMs = True
 
 
 @pytest.mark.embedding
