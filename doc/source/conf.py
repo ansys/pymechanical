66cb7486ce43e2f6d929a7b8924051e0634cbadf
klmcadams <58492561+klmcadams@users.noreply.github.com>
2024-06-17 13:32:30 -0400

    adjust linkcheck ignore url

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -301,7 +301,7 @@ linkcheck_ignore = [
     "https://ansysaccount.b2clogin.com/*",
     "https://answers.microsoft.com/en-us/windows/forum/all/*",
     "https://download.ansys.com/*",
-    "../api/ansys/mechanical/core/*",  # Remove this after release 0.10.12
+    "../api/*",  # Remove this after release 0.10.12
     "path.html",
 ]
 
