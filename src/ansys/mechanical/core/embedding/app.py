38bc8e451a949fd04905212fbf3116eb4782211f
dkunhamb <dipin.kunhambunair@ansys.com>
2024-09-05 09:30:39 -0500

    update execute script with return value

--- src/ansys/mechanical/core/embedding/app.py
+++ src/ansys/mechanical/core/embedding/app.py
@@ -227,7 +227,13 @@ class App:
         light_mode = True
         args = None
         rets = None
-        return self.script_engine.ExecuteCode(script, SCRIPT_SCOPE, light_mode, args, rets)
+        script_result = self.script_engine.ExecuteCode(script, SCRIPT_SCOPE, light_mode, args, rets)
+        if script_result is None:
+            error_msg = f"Failed to execute the script"
+            if script_result.Error is not None:
+                error_msg += f": {script_result.Error.Message}"
+            raise Exception(error_msg)
+        return str(script_result.Value)
 
     def plotter(self) -> None:
         """Return ``ansys.tools.visualization_interface.Plotter`` object."""
