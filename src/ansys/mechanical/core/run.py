7681e63cc3e25702cca62f5a55e08417a9ea6341
klmcadams <58492561+klmcadams@users.noreply.github.com>
2025-01-20 15:50:53 -0500

    updates

--- src/ansys/mechanical/core/run.py
+++ src/ansys/mechanical/core/run.py
@@ -294,8 +294,8 @@ def cli(
 ):
     """CLI tool to run mechanical.
 
-    USAGE:
-
+    Examples
+    --------
     The following example demonstrates the main use of this tool:
 
         $ ansys-mechanical -r 251 -g
