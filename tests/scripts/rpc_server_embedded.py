ce1a1dd0508c5c0094f0c88bec61361b80e2e89d
dkunhamb <dipin.kunhambunair@ansys.com>
2025-03-04 08:54:14 -0600

    rpc stand test

--- tests/scripts/rpc_server_embedded.py
+++ tests/scripts/rpc_server_embedded.py
@@ -22,13 +22,59 @@
 """Start rpc sever for embedded instance."""
 import sys
 
-from ansys.mechanical.core.embedding.rpc import MechanicalDefaultServer
+import ansys.mechanical.core as mech
+from ansys.mechanical.core.embedding.rpc import DefaultServiceMethods
+from ansys.mechanical.core.embedding.rpc.server import MechanicalEmbeddedServer
+from ansys.mechanical.core.embedding.rpc.utils import remote_method
+
+
+def change_project_name_method(app: mech.App, name: str):
+    """Change the project name of `app` to `name`."""
+    app.DataModel.Project.Name = name
+
+
+def get_project_name_method(app: mech.App):
+    return app.DataModel.Project.Name
+
+
+def get_model_name_method(app):
+    return app.Model.Name
+
+
+class ServiceMethods:
+    def __init__(self, app):
+        self._app = app
+
+    def __repr__(self):
+        return '"ServiceMethods instance"'
+
+    @remote_method
+    def get_model_name_impl(self):
+        return self._app.Model.Name
+
+    def get_model_name_no_wrapper(self):
+        return self._app.Model.Name
+
+    @remote_method
+    def change_project_name_impl(self, name: str):
+        self._app.DataModel.Project.Name = name
+
+    @remote_method
+    def get_project_name_impl(self):
+        return self.helper_func()
+
+    def helper_func(self):
+        return self._app.DataModel.Project.Name
+
 
 if __name__ == "__main__":
     _port = int(sys.argv[1])
     _version = int(sys.argv[2])
-    server = MechanicalDefaultServer(
+
+    server = MechanicalEmbeddedServer(
         port=_port,
         version=_version,
+        impl=[DefaultServiceMethods, ServiceMethods],
+        methods=[get_project_name_method, change_project_name_method, get_model_name_method],
     )
     server.start()
