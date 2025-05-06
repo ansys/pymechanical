7afa3226e736f1edeaf8b09532d458f5817f066d
klmcadams <58492561+klmcadams@users.noreply.github.com>
2025-05-06 13:13:26 -0400

    fix attribute error

--- src/ansys/mechanical/core/ide_config.py
+++ src/ansys/mechanical/core/ide_config.py
@@ -72,7 +72,7 @@ def get_stubs_versions(stubs_location: Path):
     revns = [
         int(revision[1:])
         for revision in stubs_location.iterdir()
-        if (stubs_location / revision).is_dir() and revision.startswith("v")
+        if (stubs_location / revision).is_dir() and revision.name.startswith("v")
     ]
     return revns
 
