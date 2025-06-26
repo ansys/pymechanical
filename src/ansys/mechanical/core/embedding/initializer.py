0f241f65ecd146c0430592379391e0cfa4a74565
Mohamed Koubaa <mohamed.koubaa@ansys.com>
2025-06-26 08:35:09 -0500

    fix: add more versions to lite host (#1208)
    Co-authored-by: Mohamed Koubaa <koubaa@github.com>
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- src/ansys/mechanical/core/embedding/initializer.py
+++ src/ansys/mechanical/core/embedding/initializer.py
@@ -189,7 +189,7 @@ def __set_environment(version: int) -> None:
     # Set an environment variable to use the custom CLR host
     # for embedding.
     # In the future (>251), it would always be used.
-    if version == 251:
+    if version == 251 or version == 252 or version == 261:
         if "PYMECHANICAL_NO_CLR_HOST_LITE" not in os.environ:
             os.environ["ANSYS_MECHANICAL_EMBEDDING_CLR_HOST"] = "1"
 
