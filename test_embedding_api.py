665c76e4b7d64d4613c4cb7360d04c06244a66ad
dkunhamb <dipin.kunhambunair@ansys.com>
2025-07-10 13:06:11 -0500

    add embedding

---
+++ test_embedding_api.py
@@ -0,0 +1,40 @@
+
+from ansys.mechanical.core import App
+from ansys.mechanical.core.examples import delete_downloads, download_file
+
+app = App()
+app.update_globals(globals())
+print(app)
+
+
+from ansys.mechanical.core.embedding.global_importer import Quantity
+from ansys.mechanical.core.embedding.transaction import Transaction
+
+
+model = app.Model
+
+# Create a geometry import group for the model
+geometry_import_group = model.GeometryImportGroup
+# Add the geometry import to the group
+geometry_import = geometry_import_group.AddGeometryImport()
+# Set the geometry import format
+geometry_import_format = (
+    Ansys.Mechanical.DataModel.Enums.GeometryImportPreference.Format.Automatic
+)
+# Set the geometry import preferences
+geometry_import_preferences = Ansys.ACT.Mechanical.Utilities.GeometryImportPreferences()
+geometry_import_preferences.ProcessNamedSelections = True
+
+geometry_path = download_file(
+    "example_06_bolt_pret_geom.agdb", "pymechanical", "00_basic"
+)
+
+# Import/reload the geometry from the CAD (.agdb) file using the provided preferences
+geometry_import.Import(
+    geometry_path, Ansys.Mechanical.DataModel.Enums.GeometryImportPreference.Format.Automatic, geometry_import_preferences
+)
+
+# This wont work
+geometry_import.import(
+    geometry_path, Ansys.Mechanical.DataModel.Enums.GeometryImportPreference.Format.Automatic, geometry_import_preferences
+)
