07752e784aac25c099696fca0e8a688619d220e4
Mohamed Koubaa <mohamed.koubaa@ansys.com>
2025-08-08 11:22:34 -0500

    feat: New python script option (#1256)
    Co-authored-by: Mohamed Koubaa <koubaa@github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- src/ansys/mechanical/core/embedding/rpc/client.py
+++ src/ansys/mechanical/core/embedding/rpc/client.py
@@ -69,6 +69,10 @@ class Client:
         self._error_type = Exception
         self._has_exited = False
 
+    def _get_python_script_api_version(self) -> bool:
+        """Get the python api version."""
+        return 1
+
     def __getattr__(self, attr):
         """Get attribute from the root object."""
         if hasattr(self.root, attr):
