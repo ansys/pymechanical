4d45bc03c3e77a5a81667612ce097e53d8f40b72
dkunhamb <dipin.kunhambunair@ansys.com>
2025-09-23 17:02:41 -0500

    working for 26r1

--- future/Properties/AssemblyInfo.cs
+++ future/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ansys, Inc")]
 [assembly: AssemblyProduct("Mechanical")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("24.1.0.0")]
-[assembly: AssemblyFileVersion("24.1.0.0")]
+[assembly: AssemblyVersion("26.1.0.0")]
+[assembly: AssemblyFileVersion("26.1.0.0")]
