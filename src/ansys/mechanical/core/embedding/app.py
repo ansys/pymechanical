20d629e769d12802d6983a8283f9299225d97c74
kmcadams <kerry.mcadams@ansys.com>
2025-11-11 16:23:55 -0500

    move interactive mode before building_gallery

--- src/ansys/mechanical/core/embedding/app.py
+++ src/ansys/mechanical/core/embedding/app.py
@@ -232,6 +232,9 @@ class App:
         # Get the globals dictionary from kwargs
         globals = kwargs.get("globals")
 
+        # Get the interactive mode from kwargs
+        self._interactive_mode = kwargs.get("interactive", False)
+
         # Set messages to None before BUILDING_GALLERY check
         self._messages = None
 
@@ -279,8 +282,7 @@ class App:
         else:
             additional_args = ""
 
-        self._interactive_mode = kwargs.get("interactive", False)
-        if self._version < 261 and self._interactive_mode:
+        if self._version < 261 and self.interactive:
             raise Exception("Interactive mode is only supported starting with version 261")
 
         self._prepare_interactive_mode()
@@ -337,7 +339,7 @@ class App:
         self._disposed = True
 
     def _prepare_interactive_mode(self):
-        if not self._interactive_mode:
+        if not self.interactive:
             return
         if self._version < 261:
             raise Exception("Interactive mode is only supported starting with version 261")
@@ -348,7 +350,7 @@ class App:
 
     def _handle_interactive_shell(self):
         self._started_interactive_shell = False
-        if not self._interactive_mode:
+        if not self.interactive:
             return
         shell.start_interactive_shell(self)
         self._started_interactive_shell = True
@@ -359,7 +361,7 @@ class App:
         While that pop-up is displayed, Mechanical's main thread will
         not be blocked.
         """
-        if not self._interactive_mode:
+        if not self.interactive:
             raise Exception("wait_with_dialog is only supported in interactive mode!")
         if self.version < 261:
             raise Exception("wait_with_dialog is only supported with Mechanical 261")
@@ -540,7 +542,7 @@ class App:
         >>> app.open("path/to/file.mechdat")
         >>> app.plot()
         """
-        if self._interactive_mode:
+        if self.interactive:
             raise Exception("Plotting is not allowed in interactive mode")
 
         _plotter = self.plotter(obj)
