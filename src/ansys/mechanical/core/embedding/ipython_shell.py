003ae472687369d98bb083a11b4ad27d3dc9d594
Mohamed Koubaa <koubaa@github.com>
2025-11-12 08:35:56 -0600

    try

--- src/ansys/mechanical/core/embedding/ipython_shell.py
+++ src/ansys/mechanical/core/embedding/ipython_shell.py
@@ -199,7 +199,7 @@ def post_ipython_blocks():
     # Patch IPython to delegate to your thread
     InteractiveShell.run_cell = _run_cell_in_thread.__get__(
         InteractiveShell.instance(), InteractiveShell
-    )
+    )  # type: ignore
     LOG.info("IPython now runs all cells in your dedicated thread.")
 
 
