f115efda773883836bd12d74c4dbe3e62526cf1e
kmcadams <kerry.mcadams@ansys.com>
2025-11-14 10:42:22 -0500

    ignore harmonic_acoustics

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -200,7 +200,7 @@ sphinx_gallery_conf = {
     "doc_module": "ansys-mechanical-core",
     "image_scrapers": (DynamicScraper(), "matplotlib"),
     # Files to ignore
-    "ignore_pattern": "flycheck*|fracture_analysis_contact_debonding.py",
+    "ignore_pattern": "flycheck*|fracture_analysis_contact_debonding.py|harmonic_acoustics.py",
     "thumbnail_size": (350, 350),
 }
 
