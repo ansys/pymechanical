71f0881a90878e1f2eae94dfbcb1c64a9c9e6abe
kmcadams <kerry.mcadams@ansys.com>
2025-11-24 13:25:42 -0500

    uncomment filename pattern

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -189,7 +189,7 @@ sphinx_gallery_conf = {
     "examples_dirs": ["../../examples/"],
     # path where to save gallery generated examples
     "gallery_dirs": ["examples/gallery_examples"],  # Pattern to search for example files
-    # "filename_pattern": r"\.py",
+    "filename_pattern": r"\.py",
     # Remove the "Download all examples" button from the top level gallery
     "download_all_examples": False,
     # Sort gallery example by file name instead of number of lines (default)
