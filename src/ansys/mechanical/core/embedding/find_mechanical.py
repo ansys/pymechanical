f82a48de82a1a70f74a63f1f626ea243d6aac3fc
Jesús Almenara Rescalvo <jalmenar@synopsys.com>
2026-04-13 10:20:05 +0200

    Print explicit AWP_ROOT check message

--- src/ansys/mechanical/core/embedding/find_mechanical.py
+++ src/ansys/mechanical/core/embedding/find_mechanical.py
@@ -101,6 +101,8 @@ def cli(version: int, path: str | None = None) -> tuple[int, str]:
             latest_version = max(versions_found)
             print(latest_version, str(Path(os.environ[f"AWP_ROOT{latest_version}"]) / "aisol"))
             sys.exit(0)
+    else:
+        print("No AWP_ROOT### env. variables found. Falling back to default locations". )        
 
     # Use ansys-tools-path
     _exe = atp.get_mechanical_path(allow_input=False, version=version)
